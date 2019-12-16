4eebf9e83c37359fbc19084761b62aa84c741dc6
jean sidharta <jeansidharta@gmail.com>
2019-12-15 21:29:15 -0300

    hello, world!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App () {
+export default function App () {
+  return (
+    <h1>Hello, World</h1>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
