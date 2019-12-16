6b7581b2d4816c9b8f8626605577667ebe093fea
jean sidharta <jeansidharta@gmail.com>
2019-12-16 00:36:35 -0300

    created the base API

--- src/index.js
+++ src/index.js
@@ -9,12 +9,22 @@ import { Provider } from 'react-redux'
 import MuiTheme from './MuiTheme'
 import routes from './constants/routes'
 import store from './redux/store'
+import { initializeAPI } from './api/base-api'
+import APIBaseURL from './constants/api-url'
+import { updateToken } from './redux/actions/auth'
 
 // Pages
 import Home from './pages/Home'
 
 // CSS
 import './main-style.css'
+
+initializeAPI({
+  baseURL: APIBaseURL,
+  tokenSelector: () => store.getState().auth.token,
+  tokenDispatcher: newToken => store.dispatch(updateToken(newToken)),
+  onError: message => window.alert(message)
+})
 
 function App () {
   return (
