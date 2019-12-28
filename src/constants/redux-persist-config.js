5c040c90f56cbd84c413b4c58cd88da0b88fe525
jean sidharta <jeansidharta@gmail.com>
2019-12-28 06:47:32 -0300

    initialized redux persist, while protecting the routes

---
+++ src/constants/redux-persist-config.js
@@ -0,0 +1,7 @@
+import storage from 'redux-persist/lib/storage'
+
+export default {
+  storage,
+  key: 'root',
+  whitelist: ['auth']
+}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from '@material-ui/core'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 // Custom
 import MuiTheme from './MuiTheme'
 import routes from './constants/routes'
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 import { initializeAPI } from './api/base-api'
 import APIBaseURL from './constants/api-url'
 import { updateToken } from './redux/actions/auth'
+import { requiresAuthentication, requiresNoAuthentication } from './lib/auth-checker'
 
 // Pages
 import Tabs from './pages/tabs'
@@ -33,13 +35,21 @@ function App () {
   return (
     <ThemeProvider theme={MuiTheme}>
       <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route path={[routes.tabs.map, routes.tabs.missions, routes.tabs.profile]} component={Tabs} />
-            <Route path={routes.login} component={Login} />
-            <Redirect to={routes.login} />
-          </Switch>
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <Switch>
+              <Route
+                path={[routes.tabs.map, routes.tabs.missions, routes.tabs.profile]}
+                component={requiresAuthentication(Tabs)}
+              />
+              <Route
+                path={routes.login}
+                component={requiresNoAuthentication(Login)}
+              />
+              <Redirect to={routes.login} />
+            </Switch>
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   )
