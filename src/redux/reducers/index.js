137cb3a49df5964a92da8b6e4a7fbbe7151b6f23
jean sidharta <jeansidharta@gmail.com>
2020-01-08 21:06:28 -0300

    created a new redux state named "country"

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth'
 import geolocationReducer from './geolocation'
+import countryReducer from './country'
 
 const reducers = combineReducers({
   auth: authReducer,
+  country: countryReducer,
   geolocation: geolocationReducer
 })
 
