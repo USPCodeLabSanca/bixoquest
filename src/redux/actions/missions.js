95f6a00c4b305d72fe0f5716d6816db36f55b447
jean sidharta <jeansidharta@gmail.com>
2020-01-23 23:45:46 -0300

    only render unfinished missions

--- src/redux/actions/missions.js
+++ src/redux/actions/missions.js
@@ -1,9 +1,17 @@
 import API from '../../api'
 
-export async function fetchNearbyMissions (lat, long) {
-  const { data: { data: missions } } = await API.fetchNearbyMissions(lat, long)
+export async function fetchNearbyMissions (lat, lng) {
+  const { data: { data: missions } } = await API.fetchNearbyMissions(lat, lng)
   return {
     type: 'SET_NEARBY_MISSIONS',
     nearbyMissions: missions
+  }
+}
+
+export async function completeMission (missionId, lat, lng) {
+  await API.completeMission(missionId, lat, lng)
+  return {
+    type: 'COMPLETE_MISSION',
+    missionId
   }
 }
