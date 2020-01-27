57f07662f192cf98ebdc09a6f02a3e93d8d7226a
jean sidharta <jeansidharta@gmail.com>
2020-01-27 04:32:28 -0300

    added a found-a-bug modal message

---
+++ src/components/modals/found-a-bug.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import Button from '@material-ui/core/Button'
+import { useDispatch } from 'react-redux'
+
+import { closeModal } from '../../redux/actions/modal'
+import Modal from '../modal'
+
+const style = {
+  root: 'w-full h-full flex justify-center items-center p-4 py-16',
+  card: 'p-4 bg-white rounded-lg max-h-full overflow-auto max-w-md',
+  title: 'text-2xl',
+  paragraph: 'my-4 text-justify'
+}
+
+const P = (props) => <p className={style.paragraph} {...props} />
+
+function FoundABugModal () {
+  const dispatch = useDispatch()
+
+  function submit () {
+    dispatch(closeModal())
+  }
+
+  return (
+    <Modal className={style.root}>
+      <div className={style.card}>
+        <div className={style.title}>Encontrei um bug!</div>
+        <div className={style.description}>
+          <P>Oh não! Pedimos desculpas!</P>
+          <P>
+            O BixoQuest é um jogo feito com muito amor, e nós queremos que ele seja
+            apreciado ao máximo. Por favor, ajude-nos a melhorar essa experiência
+            reportando esse bug pada nós. Se ninguam falar nada, a gente nunca vai
+            ficar sabendo!
+          </P>
+          <P>
+            Mande um E-mail para jeansidharta@gmail.com com o assunto 'bug bixoquest'.
+            Deixe uma breve descrição do seu bug na área de mensagem, e se você puder,
+            um screenshot da sua tela.
+          </P>
+          <P>
+            Agradecemos a sua colaboração, e vamos fazer o máximo para consertar isso
+            o mais rápido o possivel!
+          </P>
+        </div>
+        <Button onClick={submit} className={style.button} fullWidth variant='contained'>
+          OK
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
+export default FoundABugModal
--- src/pages/tabs/profile.js
+++ src/pages/tabs/profile.js
@@ -6,7 +6,9 @@ import Card from '@material-ui/core/Card'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 
+import { setCurrentModal } from '../../redux/actions/modal'
 import * as AuthActions from '../../redux/actions/auth'
+import FoundABugModal from '../../components/modals/found-a-bug'
 
 const style = {
   root: 'p-4 h-full',
@@ -14,12 +16,15 @@ const style = {
   avatar: { width: '30vw', height: '30vw', marginBottom: '8px' },
   userName: 'text-2xl',
   userCourse: '',
-  button: {
+  logoutButton: {
     margin: '16px 0',
     backgroundColor: '#718096',
     color: 'white',
     padding: '16px 0',
     fontSize: 16
+  },
+  generalButton: {
+    margin: '8px 0'
   }
 }
 
@@ -29,6 +34,10 @@ export default function ProfilePage () {
 
   function logout () {
     dispatch(AuthActions.logout())
+  }
+
+  function foundABug () {
+    dispatch(setCurrentModal(<FoundABugModal />))
   }
 
   return (
@@ -41,7 +50,15 @@ export default function ProfilePage () {
       <Button
         variant='contained'
         fullWidth
-        style={style.button}
+        style={style.generalButton}
+        onClick={foundABug}
+      >
+        Achou um bug?
+      </Button>
+      <Button
+        variant='contained'
+        fullWidth
+        style={style.logoutButton}
         onClick={logout}
       >
         SAIR
