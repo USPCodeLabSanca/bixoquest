f52751fa29ca8a2a26165f34121a94db5ddabfc4
jean sidharta <jeansidharta@gmail.com>
2020-01-27 04:31:29 -0300

    geolocation is now responsible to translate its own errors

--- src/redux/actions/geolocation.js
+++ src/redux/actions/geolocation.js
@@ -6,8 +6,21 @@ export function setPosition (position) {
 }
 
 export function unavailable (error) {
+  const defaultMessage = 'Ops! um erro desconhecido ocorreu. Por favor, tente novamente'
+  const errorMessages = {
+    // More details about the errors [here](https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError)
+    // User denied permission
+    1: 'Parece que este site não tem aceeso à sua localização. Por favor, verifique se as permissões estão corretas',
+    // Position unavailable
+    2: 'Tivemos um problema ao buscar sua posição. Por favor, recarregue a página e tente novamente',
+    // timeout
+    3: 'Tivemos um problema ao buscar sua posição. Por favor, recarregue a página e tente novamente'
+  }
   return {
     type: 'UNAVAILABLE',
-    error
+    error: {
+      code: error.code,
+      message: errorMessages[error.code] || defaultMessage
+    }
   }
 }
