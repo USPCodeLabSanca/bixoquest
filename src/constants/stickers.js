1db1aa1da0d8461931d56e62bc8094a6ab30142c
Andre Santana <andre_sfnd@usp.br>
2020-02-02 19:45:07 -0300

    constants/stickers.js added

---
+++ src/constants/stickers.js
@@ -0,0 +1,8 @@
+//Array of stickers
+export default [
+    'http://place-puppy.com/200x200',
+    'http://place-puppy.com/200x300',
+    'http://place-puppy.com/200x400',
+    'http://place-puppy.com/200x500',
+    'http://place-puppy.com/200x600',
+]
