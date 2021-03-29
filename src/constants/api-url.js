bb3556acb155d03aafeed518a645e02751e5cbf8
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-28 21:09:50 -0300

    Change api url to dev environment

--- src/constants/api-url.js
+++ src/constants/api-url.js
@@ -1,5 +1,6 @@
 // const API_URL = 'http://localhost:8080/'
 // const API_URL = 'https://bixoquest.icmc.usp.br/api/'
-const API_URL = 'http://192.168.0.100:8080/api/'
+const API_URL = 'https://bixoquest.icmc.usp.br/dev/api/'
+// const API_URL = 'http://192.168.0.100:8080/api/'
 
 export default API_URL
