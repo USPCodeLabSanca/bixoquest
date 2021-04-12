97382d61fc45e321de7e2f79e8309c8fd881ebac
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-11 22:39:51 -0300

    Change backend url to production

--- src/constants/api-url.js
+++ src/constants/api-url.js
@@ -1,8 +1,8 @@
 // const API_URL = 'http://localhost:8080/'
 // const API_URL = 'https://bixoquest.icmc.usp.br/api/'
-const API_URL = 'https://bixoquest.icmc.usp.br/dev/api/';
+const API_URL = 'https://bixoquest.icmc.usp.br/api/';
 export const SOCKET_URL = 'https://bixoquest.icmc.usp.br';
-export const SOCKET_PATH = '/dev/socket.io';
+export const SOCKET_PATH = '/socket.io';
 // const API_URL = 'http://192.168.0.100:8080/api/'
 
 export default API_URL;
