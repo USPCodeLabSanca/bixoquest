b96026d35a947978f72ef2c20c9b6ab8cac3f5d7
Jean Sidharta <jeansidharta@gmail.com>
2021-04-11 23:10:17 -0300

    Redux persist state reset

--- src/constants/redux-persist-config.js
+++ src/constants/redux-persist-config.js
@@ -3,6 +3,12 @@ import storage from 'redux-persist/lib/storage';
 const ReduxPersistConfig = {
 	storage,
 	key: 'root',
+	version: 2,
+	migrate: async (oldState, version) => {
+		// If new version, clear state
+		if (oldState._persist.version !== version) return {};
+		else return oldState;
+	},
 	whitelist: ['auth'],
 };
 
