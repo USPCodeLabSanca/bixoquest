29eb1a5c11363af2ea139198ba703cd1e3003ae6
jean sidharta <jeansidharta@gmail.com>
2021-04-12 23:36:41 -0300

    Created chat

--- src/pages/tabs/map/index.js
+++ src/pages/tabs/map/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 // Material-ui imports
 import Fab from '@material-ui/core/Fab';
-// import PhotoCamera from '@material-ui/icons/PhotoCamera';
-// import SwapVert from '@material-ui/icons/SwapVert';
+import Chat from '@material-ui/icons/Chat';
 import Receipt from '@material-ui/icons/Receipt';
 
 // Library imports
 import { useSelector } from 'react-redux';
-// import { useHistory } from 'react-router-dom';
 
 // Redux actions imports
 import * as ModalActions from '../../../redux/actions/modal';
@@ -19,16 +17,16 @@ import PackModal from '../../../components/modals/packet';
 
 // Images imports
 
-// MISC imports
-// import Routes from '../../../constants/routes';
-
 import PlayerOverlay from './players-overlay';
 import MissionMarkers from './mission-markers';
 import { initialPlayerPosition } from '../../../constants/initial-player-position';
 import MemorablePlaces from './memorable-places';
+import ChatContainer from './chat';
 
 const style = {
-	actionButtonsContainer: 'absolute bottom-0 right-0 mr-4 mb-16 flex flex-col',
+	root: 'w-full h-full relative',
+	floatingContainer: 'w-full h-full absolute',
+	actionButtonsContainer: 'absolute bottom-0 right-0 mr-4 mb-8 flex flex-col',
 	fab: {
 		margin: '8px 0',
 		outline: 'none',
@@ -36,39 +34,38 @@ const style = {
 };
 
 export default function MapScreen() {
-	// const history = useHistory();
+	const [isChatUp, setIsChatUp] = React.useState(false);
 	const availablePacks = useSelector(state => state.auth.user.availablePacks);
 
 	const showPack = ModalActions.useModal(() => <PackModal />);
 
-	// function giveCards() {
-	// 	history.push(Routes.giveCards);
-	// }
-
-	// function readQrCode() {
-	// 	history.push(Routes.qrcodeReader);
-	// }
+	function toggleChat() {
+		setIsChatUp(e => !e);
+	}
 
 	return (
-		<>
-			<div className={style.actionButtonsContainer} style={{ zIndex: 10000 }}>
-				{/* <Fab size="small" style={style.fab} onClick={giveCards}>
-					<SwapVert />
-				</Fab> */}
-				{availablePacks > 0 && (
-					<Fab size="small" style={style.fab} onClick={showPack}>
-						<Receipt />
+		<div className={style.root}>
+			<div
+				className={style.floatingContainer}
+				style={{ transition: 'bottom 200ms', bottom: isChatUp ? '40vh' : '0vh' }}
+			>
+				<div className={style.actionButtonsContainer} style={{ zIndex: 100000 }}>
+					{availablePacks > 0 && (
+						<Fab size="small" style={style.fab} onClick={showPack}>
+							<Receipt />
+						</Fab>
+					)}
+					<Fab size="small" style={style.fab} onClick={toggleChat}>
+						<Chat />
 					</Fab>
-				)}
-				{/* <Fab size="small" style={style.fab} onClick={readQrCode}>
-					<PhotoCamera />
-				</Fab> */}
+				</div>
+				<ChatContainer />
 			</div>
 			<Map initialConfiguration={{ center: initialPlayerPosition, zoom: 18 }}>
 				<PlayerOverlay />
 				<MissionMarkers />
 				<MemorablePlaces />
 			</Map>
-		</>
+		</div>
 	);
 }
