d720f8f1bbdcfb6918037cd82318118ab7ce59a3
jean sidharta <jeansidharta@gmail.com>
2021-04-13 02:51:39 -0300

    Added all missing memorable places

--- src/constants/memorable-places.js
+++ src/constants/memorable-places.js
@@ -4,6 +4,36 @@ const observatorioDescription =
 const blocoEDescription =
 	'O local recebe esse nome por conta de sua origem: o prédio era, antes dos anos 50, o Matadouro Municipal de São Carlos. A construção foi desativada e doada à Universidade de São Paulo para a construção da Escola de Engenharia.';
 
+const pracaRegistrumDescription =
+	'Do lado oposto ao Portal Décadas, está localizada a Praça Registrum, também criada durante as comemorações do cinquentenário da EESC. Nela, estão dispostos totens com o registro de todas as turmas de engenheiros graduados pela Escola, bem como de seus dirigentes e servidores docentes e técnico-administrativos pioneiros.';
+
+const portalDecadasDescription =
+	'O portal décadas é um monumento comemorativo dos 50 anos de EESC, cada um de seus cinco arcos representa uma das décadas do instituto até o seu ano de construção, 2001.';
+
+const numaDescription =
+	'O NUMA é uma organização que agrega atualmente alguns grupos de pesquisa do Departamento de Engenharia de Produção (SEP) da Escola de Engenharia de São Carlos (EESC) da Universidade de São Paulo (USP).';
+
+const jorgeCaronDescription =
+	'O NUMA é uma organização que agrega atualmente alguns grupos de pesquisa do Departamento de Engenharia de Produção (SEP) da Escola de Engenharia de São Carlos (EESC) da Universidade de São Paulo (USP).';
+
+const cetepeDescription =
+	'O CETEPE é o Centro de Tecnologia Educacional para Engenharia, pensado para aprimorar o ensino da engenharia. Para a realização de seus eventos, palestras, conferências, cursos, seminários etc, conta com dois auditórios: O auditório I -  Marcius Fantozzi Giorgetti se localiza no piso superior do cetepe e comporta até 192 lugares. O auditório II se localiza no piso térreo e tem a capacidade de 34 lugares.';
+
+const ceferDescription =
+	'O CEFER é o complexo esportivo do nosso campus e possui uma enorme infraestrutura, contando com 4 quadras externas (3 em atividade), duas quadras de tênis, um campo e uma quadra de areia, uma piscina semi-olímpica, um campo e um ginásio poliesportivo. Além de ser palco dos treinos e competições esportivas das nossas modalidades, o espaço também é voltado ao lazer dos nossos alunos com todo o apoio de empréstimo de materiais esportivos e reservas de quadra. É no CEFER que você marca aquele fut, joga um vôlei de praia e depois consegue curtir a piscininha com os amigos. O CEFER também é onde acontece o Handebol e Futsal do Tusquinha, nosso torneio entre Federal e CAASO envolvendo apenas bixos e bixetes.';
+
+const caasoDescription =
+	'O Centro Acadêmico “Armando de Salles Oliveira” é a entidade representativa dos estudantes universitários da USP São Carlos. Considerado um dos maiores centros acadêmicos da América latina, o CAASO é responsável por organizar eventos culturais, oficinas, debates e campanhas beneficentes. O ambiente físico do CAASO é composto pela BIBLIOTECAASO, pelo palquinho, bar do Mario, Salão cultural, salinha da Atletica, salão de jogos dentre outros';
+
+const blocoBDescription =
+	'O Bloco didático “B” é o bloco da engenharia de produção, onde estão situadas salas de aula, de professores e a sala do PRÓ-PRODUÇÃO ( A secretaria acadêmica da engenharia de produção).';
+
+const bandeijaoDescription =
+	'O restaurante universitário, conhecido como Bandejão, oferece diariamente 2 refeições (almoço e jantar) aos universitários a baixo custo (R$2,00). Em 2011 foi criado o aplicativo “Cardapio USP” onde é possível verificar o cardápio da semana. Para entrar no restaurante é preciso apresentar a carteirinha de estudante ou o aplicativo “e-card” com saldo de pelo menos 2 reais.';
+
+const aquarioDescription =
+	'O aquário é uma sala de estudos, anexo à Biblioteca Central da EESC, disponível para os alunos do Campus, com um funcionamento de 24 horas. Com disponibilidade de internet, neste local, os alunos podem estudar tanto em grupos, como também individualmente, sem se preocupar com o horário de fechamento do local.';
+
 export const memorablePlaces = [
 	{
 		name: 'SAdEM',
@@ -164,6 +194,228 @@ export const memorablePlaces = [
 				url: '/memorable-places/enactus/enactus_0.jpg',
 				description:
 					'A Enactus é uma organização internacional sem fins lucrativos dedicada a inspirar alunos a melhorar o mundo através do empreendedorismo social, visando impactar vidas a partir de 3 pilares: social, ambiental e econômico.  Para saber mais de nosso time, acesse o nosso Instagram: @enactus_csaocarlos',
+			},
+		],
+	},
+	{
+		name: 'Salinha da Atlética',
+		coords: {
+			lat: -22.006911,
+			lng: -47.897102,
+		},
+		images: [
+			{
+				url: '/memorable-places/salinha-atletica/salinha-atletica_0.jpg',
+				description:
+					'A Atlética CAASO é responsável por promover as atividades esportivas do campus, como também, competições e interações entre os atletas e a comunidade. A salinha da Atlética é o ambiente onde acontece as vendas dos produtos da mesma e as reuniões presenciais de diretoria além de ser um ponto de encontro para atletas e atleticanos.',
+			},
+		],
+	},
+	{
+		name: 'Praça Registrum',
+		coords: {
+			lat: -22.008254,
+			lng: -47.896599,
+		},
+		images: [
+			{
+				url: '/memorable-places/praca-registrum/praca-registrum_0.jpg',
+				description: pracaRegistrumDescription,
+			},
+			{
+				url: '/memorable-places/praca-registrum/praca-registrum_1.jpg',
+				description: pracaRegistrumDescription,
+			},
+		],
+	},
+	{
+		name: 'Portal das Décadas',
+		coords: {
+			lat: -22.008674,
+			lng: -47.896747,
+		},
+		images: [
+			{
+				url: '/memorable-places/portal-decadas/portal-decadas_0.jpg',
+				description: portalDecadasDescription,
+			},
+			{
+				url: '/memorable-places/portal-decadas/portal-decadas_1.jpg',
+				description: portalDecadasDescription,
+			},
+		],
+	},
+	{
+		name: 'Núcleo de Manufatura Avançada',
+		coords: {
+			lat: -22.010045,
+			lng: -47.896667,
+		},
+		images: [
+			{
+				url: '/memorable-places/numa/numa_0.jpg',
+				description: numaDescription,
+			},
+			{
+				url: '/memorable-places/numa/numa_1.jpg',
+				description: numaDescription,
+			},
+		],
+	},
+	{
+		name: 'Anfiteatro Jorge Caron',
+		coords: {
+			lat: -22.008039,
+			lng: -47.897097,
+		},
+		images: [
+			{
+				url: '/memorable-places/jorge-caron/jorge-caron_0.jpg',
+				description: jorgeCaronDescription,
+			},
+			{
+				url: '/memorable-places/jorge-caron/jorge-caron_1.jpg',
+				description: jorgeCaronDescription,
+			},
+			{
+				url: '/memorable-places/jorge-caron/jorge-caron_2.jpg',
+				description: jorgeCaronDescription,
+			},
+		],
+	},
+	{
+		name: 'E1',
+		coords: {
+			lat: -22.008407,
+			lng: -47.896619,
+		},
+		images: [
+			{
+				url: '/memorable-places/e1/e1_0.jpg',
+				description:
+					'O E1 é um dos prédios mais antigos da USP, construído na década de 50 é usado atualmente como sede administrativa da EESC, onde os coordenadores e diretores do instituto tem suas salas. A sua frente está situada a praça Registrum e do lado oposto o portal Décadas.',
+			},
+		],
+	},
+	{
+		name: 'CETEPE',
+		coords: {
+			lat: -22.00706,
+			lng: -47.895884,
+		},
+		images: [
+			{
+				url: '/memorable-places/cetepe/cetepe_0.jpg',
+				description: cetepeDescription,
+			},
+			{
+				url: '/memorable-places/cetepe/cetepe_1.jpg',
+				description: cetepeDescription,
+			},
+			{
+				url: '/memorable-places/cetepe/cetepe_2.jpg',
+				description: cetepeDescription,
+			},
+		],
+	},
+	{
+		name: 'CEFER',
+		coords: {
+			lat: -22.005751,
+			lng: -47.897112,
+		},
+		images: [
+			{
+				url: '/memorable-places/cefer/cefer_0.jpg',
+				description: ceferDescription,
+			},
+			{
+				url: '/memorable-places/cefer/cefer_1.jpg',
+				description: ceferDescription,
+			},
+			{
+				url: '/memorable-places/cefer/cefer_2.jpg',
+				description: ceferDescription,
+			},
+			{
+				url: '/memorable-places/cefer/cefer_3.jpg',
+				description: ceferDescription,
+			},
+		],
+	},
+	{
+		name: 'CAASO',
+		coords: {
+			lat: -22.006791,
+			lng: -47.896794,
+		},
+		images: [
+			{
+				url: '/memorable-places/caaso/caaso_0.jpg',
+				description: caasoDescription,
+			},
+			{
+				url: '/memorable-places/caaso/caaso_1.jpg',
+				description: caasoDescription,
+			},
+			{
+				url: '/memorable-places/caaso/caaso_2.jpg',
+				description: caasoDescription,
+			},
+		],
+	},
+	{
+		name: 'Bloco B',
+		coords: {
+			lat: -22.00352,
+			lng: -47.5348,
+		},
+		images: [
+			{
+				url: '/memorable-places/bloco-b/bloco-b_0.jpg',
+				description: blocoBDescription,
+			},
+			{
+				url: '/memorable-places/bloco-b/bloco-b_1.jpg',
+				description: blocoBDescription,
+			},
+			{
+				url: '/memorable-places/bloco-b/bloco-b_2.jpg',
+				description: blocoBDescription,
+			},
+		],
+	},
+	{
+		name: 'Bandeijão',
+		coords: {
+			lat: -22.006561,
+			lng: -47.896929,
+		},
+		images: [
+			{
+				url: '/memorable-places/bandeijao/bandeijao_0.jpg',
+				description: bandeijaoDescription,
+			},
+			{
+				url: '/memorable-places/bandeijao/bandeijao_1.jpg',
+				description: bandeijaoDescription,
+			},
+		],
+	},
+	{
+		name: 'Aquário',
+		coords: {
+			lat: -22.007097,
+			lng: -47.896138,
+		},
+		images: [
+			{
+				url: '/memorable-places/aquario/aquario_0.jpg',
+				description: aquarioDescription,
+			},
+			{
+				url: '/memorable-places/aquario/aquario_1.jpg',
+				description: aquarioDescription,
 			},
 		],
 	},
